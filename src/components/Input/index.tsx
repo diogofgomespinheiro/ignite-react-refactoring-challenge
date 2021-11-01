bf55f59987d64999dffac06736ef684b1f21ac0e
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2021-11-01 23:08:50 +0000

    📦 refactor: convert Input Component to ts

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,14 +3,21 @@ import {
   useRef,
   useState,
   useCallback,
+  ComponentPropsWithoutRef,
+  FunctionComponent
 } from 'react';
 
 import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+type InputProps = {
+  name: string;
+  icon: FunctionComponent<{ size: number }>;
+} & ComponentPropsWithoutRef<'input'>;
+
+const Input = ({ name, icon: Icon, ...rest }: InputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -31,7 +38,7 @@ const Input = ({ name, icon: Icon, ...rest }) => {
     registerField({
       name: fieldName,
       ref: inputRef.current,
-      path: 'value',
+      path: 'value'
     });
   }, [fieldName, registerField]);
 
